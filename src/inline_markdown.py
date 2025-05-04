f71a91183f5570c5fa672822ec77c18c9dd47224
Bul8a54ur <tairshechter1118@gmail.com>
2025-05-04 08:33:43 +0300

    split nodes image

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -32,3 +32,41 @@ def extract_markdown_images(text):
 
 def extract_markdown_links(text):
     return re.findall(r" \[(.*?)\]\((.*?)\)", text)
+
+def split_nodes_image(old_nodes):
+    new_nodes = []
+
+    for old_node in old_nodes:
+        if old_node.text_type != TextType.TEXT:
+            new_nodes.append(old_node)
+            continue
+
+        image_nodes_extracted = extract_markdown_images(old_node.text)
+
+        if not image_nodes_extracted:
+            new_nodes.append(old_node)
+            continue
+
+        current_text = old_node.text
+
+        for image_node in image_nodes_extracted:
+            image_alt = image_node[0]
+            image_link = image_node[1]
+            sections = current_text.split(f"![{image_alt}]({image_link})", 1)
+
+            if sections[0]:
+                new_nodes.append(TextNode(sections[0], TextType.TEXT))
+
+            new_nodes.append(TextNode(image_alt, TextType.IMAGE, image_link))
+
+            if len(sections) > 1:
+                current_text = sections[1]
+            else:
+                current_text = ""
+        
+        if current_text:
+            new_nodes.append(TextNode(current_text, TextType.TEXT))
+
+    return new_nodes
+
+# def split_nodes_link(old_nodes):
--- src/main.py
+++ src/main.py
@@ -3,11 +3,15 @@ from textnode import *
 from inline_markdown import *
 
 def main():
-    text = "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)"
-    print(extract_markdown_images(text))
-
-    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
-    print(extract_markdown_links(text))
+    texts = [
+        "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+        "This is text with an ![image](https://i.imgur.com/zjjcJKZ.png) and another ![second image](https://i.imgur.com/3elNhQu.png)",
+        "![image](https://i.imgur.com/zjjcJKZ.png)",
+        "",
+        "This is just a text"
+    ]
+    for text in texts:
+        print(f".\n{split_nodes_image([TextNode(text, TextType.TEXT)])}")
 
 if __name__=="__main__":
     main()
